ec22d8f0e6b5275a0632331b65a86581c6baa05e
Sandarva Podder <sandarvapodder09032004@gmail.com>
2025-09-06 16:50:53 +0530

    iframr csp header

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,11 @@ const app = express();
 
 // Serve static files from the 'assets' directory
 // app.use('/assets', express.static(path.join(process.cwd(), 'assets')));
-
+app.use((req, res, next) => {
+  res.setHeader("X-Frame-Options", "ALLOWALL"); // or "ALLOW-FROM <your-domain>"
+  res.setHeader("Content-Security-Policy", "frame-ancestors *");
+  next();
+});
 // app.get("/{*splat}", async (req, res) => {
 app.get("/:id/{*splat}", async (req, res) => {
   // const host = req.params;
@@ -43,12 +47,6 @@ app.get("/:id/{*splat}", async (req, res) => {
 
   // Use mime-types to determine the correct content type
   const type = mime.lookup(filePath) || "application/octet-stream";
-  // const type = filePath.endsWith("html")
-  //   ? "text/html"
-  //   : filePath.endsWith("css")
-  //   ? "text/css"
-  //   : "application/javascript";
-  // res.set("Content-Type", type);
   res.setHeader("Content-Type", type);
 
   res.send(contents.Body);
